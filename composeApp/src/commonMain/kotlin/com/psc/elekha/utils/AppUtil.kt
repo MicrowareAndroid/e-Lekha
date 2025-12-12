37f20a87a2ab9b59f573a57f8a53fd215146671a
Ujjwal Sharma <developer.ujjwalsharma@gmail.com>
2025-12-12 18:27:34 +0530

    12/12/2025,resolve conflict

--- composeApp/src/commonMain/kotlin/com/psc/elekha/utils/AppUtil.kt
+++ composeApp/src/commonMain/kotlin/com/psc/elekha/utils/AppUtil.kt
@@ -2617,13 +2617,11 @@ fun LabelValueText(label: String, value: String,) {
     ) {
         ReusableTextViewGrayCard(
             text = label,
-<<<<<<< HEAD
-            fontSize = 13
-=======
+
+
 
             fontSize = 13,
 
->>>>>>> 59cfe84f67d95e2384208fbd43b9703ca7558da8
 
       )
 
@@ -2633,16 +2631,12 @@ fun LabelValueText(label: String, value: String,) {
         Spacer(modifier = Modifier.width(4.dp))
         ReusableTextViewBlackCard(
             text = value,
-<<<<<<< HEAD
-            fontSize = 12
+            fontSize = 12,
 
 
 
             )
-=======
-            fontSize = 12,
-        )
->>>>>>> 59cfe84f67d95e2384208fbd43b9703ca7558da8
+
     }
 }
 
@@ -2743,15 +2737,12 @@ fun FormFieldCompacts(
 
     Column(modifier) {
 
-<<<<<<< HEAD
-=======
 
 
 
         Spacer(Modifier.height(5.dp))
 
 
->>>>>>> 59cfe84f67d95e2384208fbd43b9703ca7558da8
         Box(
             modifier = Modifier
                 .fillMaxWidth()
