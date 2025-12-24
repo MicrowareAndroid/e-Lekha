f059cd3b7dd8d95bb1cb10e61dcc5101f0b22113
Mohd Mashkoor <mashkoor54@gmail.com>
2025-12-24 12:08:33 +0530

    Add Table Columns

---
+++ composeApp/src/commonMain/kotlin/com/psc/elekha/database/entity/CustomerMovableAssetsEntity.kt
@@ -0,0 +1,18 @@
+package com.psc.elekha.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "CustomerMovableAssets")
+data class CustomerMovableAssetsEntity(
+    @PrimaryKey @ColumnInfo(name = "MAGUID") val MAGUID: String,
+    @ColumnInfo(name = "GUID") val GUID: String?,
+    @ColumnInfo(name = "VehicleID") val VehicleID: Int?,
+    @ColumnInfo(name = "VehicleNo") val VehicleNo: String?,
+    @ColumnInfo(name = "VehicleImageName") val VehicleImageName: String?,
+    @ColumnInfo(name = "IsUpload") val IsUpload: Int?,
+    @ColumnInfo(name = "IsEdited") val IsEdited: Int?,
+    @ColumnInfo(name = "IsDeleted") val IsDeleted: Int?
+
+)
